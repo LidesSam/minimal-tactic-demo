bc8b281a0f7e40b503fe0098801cc8fe3dbe814e
LidesSam <lides.sam@gmail.com>
2024-06-04 00:06:45 -0300

    new freature: open close turn menu

--- source/Screens/worldStates/playerturn.gd
+++ source/Screens/worldStates/playerturn.gd
@@ -1,7 +1,9 @@
 extends "res://addons/fsmgear/source/FsmState.gd"
 
 var endstate=false
-
+var onTurnMenu=false
+var onConfirm=false
+var mode=""
 func _ready():
 	super()
 	
@@ -9,8 +11,9 @@ func _ready():
 
 func enter(actowner):
 	super(actowner)
-	
+	onTurnMenu=false
 	actowner.cursor.canMove=true
+	onConfirm=false
 
 func update(actowner,delta):
 	
@@ -21,11 +24,31 @@ func handleInput(actowner,event):
 			if(actowner.select_hover_unit()):
 				actowner.display_unit_actions()
 	actowner.reset_hover_unit()
+	
+	if Input.is_action_just_pressed("ui_accept"):
+		
+		if(!onConfirm and onTurnMenu):
+			actowner.turnActMenu.hide()
+			onTurnMenu=false
+		else:
+			actowner.turnActMenu.show()
+			onTurnMenu=true
+	if Input.is_action_just_pressed("ui_back"):
+		if(onConfirm):
+			pass
+		else:
+			mode = actowner.turnActMenu.get_current_action()
+			match(mode):
+				_:
+					pass
+			actowner.turnActMenu.hide()
 	pass
 
 func state_ended():
 	return endstate
-
+func exit(actowner):
+	actowner.turnActMenu.hide()
+	onTurnMenu=false
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
