a9c7cde5f3dd4a33b5c6acbc540481dfee1dd80d
LidesSam <lides.sam@gmail.com>
2024-08-22 01:15:47 -0300

    fix: fixed cursor option set, after move

--- source/Objects/ActionList.gd
+++ source/Objects/ActionList.gd
@@ -15,52 +15,64 @@ func _ready():
 		lbl.horizontal_alignment = HORIZONTAL_ALIGNMENT_RIGHT
 		lbl.vertical_alignment = VERTICAL_ALIGNMENT_CENTER
 		lbl.text = defOp
-#		lbl.align = Label.ALIGNMENT_CENTER
 		$options.add_child(lbl)
 	update_cursor_pos()
 	pass # Replace with function body.
 
 func set_actions_from_units(unit):
-	actionsEnabled=unit.actEnabled
-	var i=0	
+	actionsEnabled = unit.actEnabled
+	var i = 0	
 	for e in actionsEnabled:
-		if(e):
-			print("+")
-			print($options.get_child(i).text)
+		if e:
 			$options.get_child(i).show()
 		else:
-			print("-")
-			print($options.get_child(i).text)
 			$options.get_child(i).hide()
-		i+=1
+		i += 1
+	set_on_first_enabled()  # Set the cursor to the first enabled option
+	update_cursor_pos()
+
+func set_on_first_enabled():
+	op=0
+	for i in range(actionsEnabled.size()):
+		if actionsEnabled[i]:
+			op = i
+			print("i:",i)
+			return
 
 func _process(delta):
 	if(visible):
 		if(Input.is_action_just_pressed("ui_up")):
-			move_up()
+			prev_action()
 		if(Input.is_action_just_pressed("ui_down")):
-			move_down()
+			next_action()
+	$Label.text=str(op)
+	var optionLBL= $options.get_child(op)
 
-func move_down():
+	$Label2.text=optionLBL.text
+
+func next_action():
 	op+=1
 	if op>lastOp:
 		op=0
-		if(!actionsEnabled[op]):
-			move_down()
+	if(!actionsEnabled[op]):
+		next_action()
 	update_cursor_pos()
+	
 
-func move_up():
+func prev_action():
 	op-=1
 	if op<0:
 		op=2
-		if(!actionsEnabled[op]):
-			move_up()
+	if(!actionsEnabled[op]):
+		prev_action()
 	update_cursor_pos()
 
 func update_cursor_pos():
+	print("op",op)
 	var opLBL= $options.get_child(op)
+	print("oplbl",opLBL," txt:",opLBL.text)
 	cursor.position = opLBL.get_begin()+Vector2(16,opLBL.get_line_height()/2)
-
+	
 func get_current_action():
 	hide()
 	var optionLBL= $options.get_child(op)
@@ -69,3 +81,9 @@ func get_current_action():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
+
+
+func _on_options_sort_children():
+	#adjsut cursor position after sort options
+	#particulary when a options is disable(hide)
+	update_cursor_pos()
